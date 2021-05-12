6f89dd9f10ef78ce31797150f4eb1aab3cc0da7c
Juan <pront23@gmail.com>
2021-05-13 00:44:35 +0900

    added log.info functions

--- mod.ts
+++ mod.ts
@@ -1,13 +1,16 @@
 import * as log from 'https://deno.land/std/log/mod.ts';
+import * as _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
 
 interface Launch {
   flightNumber: number;
   mission: string;
+  rocket: string;
+  customers: Array<string>;
 }
 
 const launches = new Map<number, Launch>();
 
-async function downloadLaunchData() {
+export async function downloadLaunchData() {
   log.info('Downloading launch data...');
   const response = await fetch('https://api.spacexdata.com/v3/launches', {
     method: 'GET',
@@ -21,9 +24,16 @@ async function downloadLaunchData() {
   const launchData = await response.json();
 
   for (const launch of launchData) {
+    const payloads = launch['rocket']['second_stage']['payloads'];
+    const customers = _.flatMap(payloads, (payload: { [x: string]: any }) => {
+      return payload['customers'];
+    });
+
     const flightData = {
       flightNumber: launch['flight_number'],
       mission: launch['mission_name'],
+      rocket: launch['rocket']['rocket_name'],
+      customers,
     };
     launches.set(flightData.flightNumber, flightData);
 
@@ -31,4 +41,8 @@ async function downloadLaunchData() {
   }
 }
 
-await downloadLaunchData();
+if (import.meta.main) {
+  await downloadLaunchData();
+  log.info(JSON.stringify(import.meta));
+  log.info(`Downloaded data for ${launches.size} Space X launches.`);
+}
